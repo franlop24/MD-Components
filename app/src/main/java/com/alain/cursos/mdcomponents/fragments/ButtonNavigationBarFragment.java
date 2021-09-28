b0c67920931b01833de518f9600f3f95c588b11f
Francisco Lopez <franlopbri@gmail.com>
2021-09-28 17:10:51 -0500

    Bottom Navigation - Personalizacion y Badge

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/ButtonNavigationBarFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/ButtonNavigationBarFragment.java
@@ -1,17 +1,21 @@
 package com.alain.cursos.mdcomponents.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class ButtonNavigationBarFragment extends Fragment {
@@ -21,6 +25,8 @@ public class ButtonNavigationBarFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUnbinder;
+    @BindView(R.id.bottomNavigation)
+    BottomNavigationView bottomNavigation;
 
     public static Component getmInstance() {
         mInstance = new Component();
@@ -39,6 +45,27 @@ public class ButtonNavigationBarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button_navigation_bar, container, false);
+        View view = inflater.inflate(R.layout.fragment_button_navigation_bar, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        bottomNavigation.getOrCreateBadge(R.id.action_start);
+        //bottomNavigation.removeBadge(R.id.action_start);
+
+        BadgeDrawable favoriteBadge = bottomNavigation.getOrCreateBadge(R.id.action_favorites);
+        favoriteBadge.setNumber(21);
+
+        BadgeDrawable profileBadge = bottomNavigation.getOrCreateBadge(R.id.action_profile);
+        profileBadge.setNumber(999);
+        profileBadge.setMaxCharacterCount(3);
+        profileBadge.setBackgroundColor(Color.CYAN);
+        profileBadge.setBadgeTextColor(Color.MAGENTA);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
     }
 }
