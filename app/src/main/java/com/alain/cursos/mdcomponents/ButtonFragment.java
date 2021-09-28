caddf67a245a8062b32ce3c292fe12a428db3e82
Francisco Lopez <franlopbri@gmail.com>
2021-09-28 13:26:11 -0500

    MaterialButton - Personalizacion

--- app/src/main/java/com/alain/cursos/mdcomponents/ButtonFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/ButtonFragment.java
@@ -2,15 +2,19 @@ package com.alain.cursos.mdcomponents;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 
 /**
@@ -22,11 +26,13 @@ public class ButtonFragment extends Fragment {
 
     private static Component mInstance;
 
+    Unbinder mUnbinder;
+
     public ButtonFragment() {
         // Required empty public constructor
     }
 
-    public static Component getmInstance(){
+    public static Component getmInstance() {
         mInstance = new Component();
         mInstance.setName(TAG);
         mInstance.setPhotoRes(R.drawable.img_button);
@@ -38,7 +44,19 @@ public class ButtonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false);
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @OnClick(R.id.btnEnable)
+    public void onClick() {
+        Toast.makeText(getActivity(), R.string.status_enabled, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        mUnbinder.unbind();
+    }
 }
