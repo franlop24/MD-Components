21f28704e638f2658cb0fb167156dbbd3297b9da
Francisco Lopez <franlopbri@gmail.com>
2021-10-02 15:19:22 -0500

    TextFields - TextInputLayout y TextInputEditText

--- app/src/main/java/com/alain/cursos/mdcomponents/utils/CommonUtils.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/utils/CommonUtils.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import com.alain.cursos.mdcomponents.fragments.ButtonFragment;
 import com.alain.cursos.mdcomponents.fragments.ButtonNavigationBarFragment;
 import com.alain.cursos.mdcomponents.fragments.SnackBarFragment;
+import com.alain.cursos.mdcomponents.fragments.TextFieldFragment;
 
 public class CommonUtils {
 
@@ -31,6 +32,10 @@ public class CommonUtils {
             //SCROLL
             case ButtonFragment.TAG:
                 fragment = new ButtonFragment();
+                break;
+
+            case TextFieldFragment.TAG:
+                fragment = new TextFieldFragment();
                 break;
 
             //STATIC
