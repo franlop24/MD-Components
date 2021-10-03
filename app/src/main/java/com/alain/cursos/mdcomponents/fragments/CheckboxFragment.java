ed93ff02f37b02ef970bb50c93c8801ff5f57681
Francisco Lopez <franlopbri@gmail.com>
2021-10-03 14:56:11 -0500

    Checkbox indeterminate

--- app/src/main/java/com/alain/cursos/mdcomponents/fragments/CheckboxFragment.java
+++ app/src/main/java/com/alain/cursos/mdcomponents/fragments/CheckboxFragment.java
@@ -1,17 +1,22 @@
 package com.alain.cursos.mdcomponents.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.alain.cursos.mdcomponents.R;
 import com.alain.cursos.mdcomponents.utils.Component;
 import com.alain.cursos.mdcomponents.utils.Constants;
+import com.buildware.widget.indeterm.IndeterminateCheckBox;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 
@@ -21,6 +26,12 @@ public class CheckboxFragment extends Fragment {
     private static Component mInstance;
 
     Unbinder mUnbinder;
+    @BindView(R.id.cbEnable)
+    CheckBox cbEnable;
+    @BindView(R.id.cbEnableIndeterminate)
+    IndeterminateCheckBox cbEnableIndeterminate;
+    @BindView(R.id.tvEnableIndeterminate)
+    TextView tvEnableIndeterminate;
 
     public static Component getmInstance() {
         mInstance = new Component();
@@ -38,6 +49,23 @@ public class CheckboxFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkbox, container, false);
+        View view = inflater.inflate(R.layout.fragment_checkbox, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        cbEnable.setOnClickListener(view1 -> {
+            String status = cbEnable.isChecked() ? "Activo" : "Inactivo";
+            Toast.makeText(getActivity(), status, Toast.LENGTH_SHORT).show();
+
+            cbEnableIndeterminate.setIndeterminate(cbEnable.isChecked());
+            tvEnableIndeterminate.setText(cbEnable.isChecked()? "Enable Indeternimate": "Disable Indeterminate");
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
     }
 }
